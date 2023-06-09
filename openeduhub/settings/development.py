acc2b1ff051a33a1581fda993e5ba4ff99f9e1c0
Rashid Mahmood <codewithrashid@gmail.com>
2023-06-09 07:53:14 +0400

    - configure postgres as backend database - update requirements to psycopg2 dependency for postgres

--- openeduhub/settings/development.py
+++ openeduhub/settings/development.py
@@ -16,8 +16,12 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'openeduhub',
+        'USER': 'rashid',
+        'PASSWORD': 'rashid',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
